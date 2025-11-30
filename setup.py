c9dd83136289a143039a2601a0aaf5315d23173d
R-Umaria <rumaria6371@conestogac.on.ca>
2025-11-29 22:27:38 -0500

    refactor: update CI workflows and setup.py for improved packaging and installation

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ setup(
     version="1.0.0",
     author="Rishi Umaria",
     author_email="rishiumaria24@gmail.com",
-    description="A simple calculator package from Week 10.",
+    description="A simple calculator package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/R-umaria/sq3_awesomecalculator_week10",
-    
+
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+
+    include_package_data=True,
+    zip_safe=False,
 
     classifiers=[
         "Programming Language :: Python :: 3",
